9900fb58942cc7ce1fe7bf7d3bcd25687bc15ffc
Sanu Vithanage <sanuv9683@gmail.com>
2017-05-26 18:46:32 +0530

    comiy

---
+++ OpenQbit.MasterdbSystem.git/OpenQbit.Masterdb.Common.Models/Master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Common.Models
+{
+    public class Master
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
