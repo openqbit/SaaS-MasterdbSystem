08c76ec09e57cc551f435ce84dd0c2e45229bef4
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 22:07:42 +0530

    Presentation Model Aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Presentation.Web/Models/Resorce/ResorceItems.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenQbit.Masterdb.Presentation.Web.Models.Resorce
+{
+    public class ResorceItems
+    {
+    }
+}
