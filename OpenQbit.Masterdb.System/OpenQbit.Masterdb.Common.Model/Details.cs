f9d7d24027ffd743c27433f36c26abc8baee56bf
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 17:37:31 +0530

    Details Model Aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Model/Details.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Common.Model
+{
+    public  class Details
+    {
+        public int ID { get; set; }
+
+        public int RID { get; set; }
+
+        public int PRID { get; set; }
+
+        public int ResourceHierachyID { get; set; }
+
+
+        public virtual Resorce R { get; set; }
+        public virtual ResourceHierachy ResourceHierachy { get; set; }
+    }
+}
