05402e0b1ab3af6745f0c8e62c16910cf328be55
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 21:19:04 +0530

    Unit test aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.BusinessService/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenQbit.Masterdb.Test.BusinessService
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
