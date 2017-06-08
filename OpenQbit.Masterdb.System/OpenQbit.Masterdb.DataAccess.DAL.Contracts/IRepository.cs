08768b3c971d0e7ac675c7996753230c1736fda6
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 19:32:01 +0530

    IRepository created

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.DataAccess.DAL.Contracts/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.DataAccess.DAL.Contracts
+{
+   public class IRepository
+    {
+    }
+}
