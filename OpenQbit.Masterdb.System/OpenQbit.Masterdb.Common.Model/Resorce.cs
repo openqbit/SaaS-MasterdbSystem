45f2b7153ae431f43e8acbc19a73d42b6fafea88
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 17:39:03 +0530

    Resouces Model Aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Model/Resorce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Common.Model
+{
+     public class Resorce
+    {
+        public int ID { get; set; }
+        public int TypeID { get; set; }
+
+        public virtual ResourceType Type { get; set; }
+
+        [Column(TypeName = "xml")]
+        public string DetailsXml { get; set; }
+
+        [NotMapped]
+        public XDocument DetailsElement
+        {
+            get { return DetailsXml != null ? XDocument.Parse(DetailsXml) : null; }
+            set { DetailsXml = value.ToString(); }
+        }
+    }
+}
