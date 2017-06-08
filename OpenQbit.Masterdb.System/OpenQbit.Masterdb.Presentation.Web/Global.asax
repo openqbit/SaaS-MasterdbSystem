fb796b29c7fe18d4d1a82c86518b63894e266bfa
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 18:19:39 +0530

    Presentation Layer Created

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Presentation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQbit.Masterdb.Presentation.Web.MvcApplication" Language="C#" %>
---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.Masterdb.Presentation.Web.Startup))]
+namespace OpenQbit.Masterdb.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
