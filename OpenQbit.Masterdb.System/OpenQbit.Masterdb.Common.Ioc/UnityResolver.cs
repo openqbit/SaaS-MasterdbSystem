750b554ba10a0975bd312a53ec0d7cb727764675
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 22:28:20 +0530

    Unity Done

--- OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Ioc/UnityResolver.cs
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Ioc/UnityResolver.cs
@@ -1,22 +1,27 @@
-﻿using System;
+﻿using OpenQbit.Masterdb.BusinessService;
+using OpenQbit.Masterdb.Common.Model;
+using OpenQbit.Masterdb.Common.Utils.Logs;
+using OpenQbit.Masterdb.DataAccess.DAL.Contracts;
+using OpenQbit.Masterdb.DataAccsess.DAL;
+using System;
 using System.Collections.Generic;
-using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
 
 namespace OpenQbit.Masterdb.Common.Ioc
 {
    public class UnityResolver
     {
-        private static readonly IUnityContainer Container = new EntityContainer();
+        private static readonly IUnityContainer Container = new UnityContainer();
 
         public static void Register()
         {
             Container.RegisterType<IRepository, Repository>();
             Container.RegisterType<ILogger, LoggerB>();
 
-            Container.RegisterType<Res, InvoiceManger>();
+            Container.RegisterType<Resorce, ResourceManger>();
         }
 
         public static T Resolve<T>()
