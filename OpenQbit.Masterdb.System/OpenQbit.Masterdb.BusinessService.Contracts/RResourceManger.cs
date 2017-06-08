aca9f6e7aee9cf2a3c280b92da59f1602136fb0c
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 20:39:35 +0530

    Bll Implementation

--- OpenQbit.Masterdb.System/OpenQbit.Masterdb.BusinessService.Contracts/RResourceManger.cs
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.BusinessService.Contracts/RResourceManger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQbit.Masterdb.Common.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace OpenQbit.Masterdb.BusinessService.Contracts
 {
-    public class Class1
+   public interface RResourceManger
     {
+        bool RecoredInvoice(Resorce resource);
     }
 }
--- OpenQbit.Masterdb.System/OpenQbit.Masterdb.BusinessService/ResourceManger.cs
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.BusinessService/ResourceManger.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using OpenQbit.Masterdb.BusinessService.Contracts;
+using OpenQbit.Masterdb.Common.Utils.Logs;
+using OpenQbit.Masterdb.DataAccess.DAL.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQbit.Masterdb.Common.Model;
 
 namespace OpenQbit.Masterdb.BusinessService
 {
-    class ResourceManger
+   public class ResourceManger : RResourceManger
     {
+        private IRepository _repository;
+
+        private ILogger _log;
+
+        [InjectionConstructor]    // Constructore  inject
+        public InvoiceManger(IRepository repository, ILogger log)
+        {
+            this._repository = repository;
+            this._log = log;
+        }
+
+
+        [Dependency] //for setters
+        public IRepository Repository
+        {
+            get { return _repository; }
+            set { _repository = value; }
+        }
+
+        [Dependency] //for setters
+        public ILogger Logger
+        {
+            get { return _log; }
+            set { _log = value; }
+        }
+
+
+        [InjectionMethod] // for methodes
+        public void SetRepository(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool RecoredInvoice(Invoice invoice) //Add Invoice
+        {
+            _log.LogError("");
+
+            return _repository.Create<Invoice>(invoice);
+
+            //throw new NotImplementedException();
+        }
+
+        public bool RecoredInvoice(Resorce resource)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
