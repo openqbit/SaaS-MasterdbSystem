537656905e01af4a93df1911fbfff3c92bc34fd1
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 19:18:41 +0530

    Logger Class Aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Utils.Logs/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Common.Utils.Logs
+{
+    public class Logger : ILogger
+    {
+        public bool LogError(string err)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
