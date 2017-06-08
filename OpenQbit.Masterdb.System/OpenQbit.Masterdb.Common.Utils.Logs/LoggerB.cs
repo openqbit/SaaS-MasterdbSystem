a000f7ead3408031bc6f60683ad27a2d8c272e9f
Sanu Vithanage <Sanu Vithanage@SOFTLOF>
2017-06-08 19:20:05 +0530

    LoggerB Completed

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Common.Utils.Logs/LoggerB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Common.Utils.Logs
+{
+   public class LoggerB : ILogger
+    {
+
+        public bool LogError(string err)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
