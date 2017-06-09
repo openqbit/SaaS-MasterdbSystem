05d1630805ade51b9d16599428bb42f78924f92c
abirandi gamage <abirandigamage96@gmail.com>
2017-06-09 11:28:20 +0530

    Test Aded

---
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.DAL/DetailsText-Dal.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQbit.Masterdb.Common.Model;
+using OpenQbit.Masterdb.DataAccsess.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Masterdb.Test.DAL
+{
+    public class DetailsText_Dal
+    {
+        [TestClass]
+        public class DALTest
+        {
+            public DALTest()
+            {
+
+            }
+            private TestContext testContextInstance;
+
+            public TestContext TestContext
+            {
+                get
+                {
+                    return testContextInstance;
+                }
+                set
+                {
+                    testContextInstance = value;
+                }
+            }
+
+            [TestMethod]
+            public void TestMethod1()
+            {
+                int expected = 10;
+
+                int actual = 10;
+
+                Assert.AreEqual(expected, actual);
+            }
+
+
+            [TestMethod]
+            public void CustomerInsertTest()
+            {
+                MasterDBContext db = new MasterDBContext();
+                Details newDetail =
+                  new Details
+                  { ID = 10, RID = 1000, PRID = 10, ResourceHierachyID = 100 };
+
+                db.Details.Add(newDetail);
+                db.SaveChanges();
+
+                Details findDetails =
+                    db.Details.Where(C => C.ID == 10).FirstOrDefault();
+
+                Assert.AreEqual(newDetail.ID, findDetails.ID);
+
+                if (findDetails != null)
+                {
+                    db.Details.Remove(findDetails);
+                }
+
+            }
+        }
+    }
+}
