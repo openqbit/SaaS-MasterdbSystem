0c0d78a9a5cebf148522296350654118d6070a95
Poorni Dananjana <poornik@openqubit.com>
2017-06-08 22:33:16 -0700

    ok

--- OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.BusinessService/ServiceTest.cs
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.BusinessService/ServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace OpenQbit.Masterdb.Test.BusinessService
 {
-    class ServiceTest
+    [TestClass]
+    public class ServiceTest
     {
+        public ServiceTest()
+        {
+
+        }
     }
 }
