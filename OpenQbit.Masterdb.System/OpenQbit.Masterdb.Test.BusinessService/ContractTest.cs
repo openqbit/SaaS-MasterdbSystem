dd1a31f579e2f35a802698db22b45a719c409282
Dilshan Chathuranga <dilimadz@gmail.com>
2017-06-09 11:01:38 +0530

    Ok

--- OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.BusinessService/ContractTest.cs
+++ OpenQbit.Masterdb.System/OpenQbit.Masterdb.Test.BusinessService/ContractTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace OpenQbit.Masterdb.Test.BusinessService
 {
-    class ContractTest
+    [TestClass]
+    public class ContractTest
     {
+        public ContractTest()
+        {
+
+        }
+        private TestContext testContextInstance;
     }
 }
